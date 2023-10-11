769bc29ed7892c743025c81d18f11c6d959b2d57
sajaabuammer <sabua2@uic.edu>
2023-10-11 13:10:08 -0500

    Saja_Update_10/11_A6

---
+++ NewLandfill_A4/Assets/IgnoreCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollider : MonoBehaviour
+{ 
+    public GameObject WOOOL;
+    public GameObject NYLLON;
+    public GameObject COOTTON;
+    public GameObject LEAATHER;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreCollision(WOOOL.GetComponent<Collider>(), GetComponent<Collider>());
+        Physics.IgnoreCollision(WOOOL.GetComponent<Collider>(), NYLLON.GetComponent<Collider>());
+        Physics.IgnoreCollision(NYLLON.GetComponent<Collider>(), GetComponent<Collider>());
+        Physics.IgnoreCollision(COOTTON.GetComponent<Collider>(), NYLLON.GetComponent<Collider>());
+        Physics.IgnoreCollision(COOTTON.GetComponent<Collider>(), GetComponent<Collider>());
+        Physics.IgnoreCollision(COOTTON.GetComponent<Collider>(), WOOOL.GetComponent<Collider>());
+        Physics.IgnoreCollision(LEAATHER.GetComponent<Collider>(), GetComponent<Collider>());
+        Physics.IgnoreCollision(LEAATHER.GetComponent<Collider>(), WOOOL.GetComponent<Collider>());
+        Physics.IgnoreCollision(LEAATHER.GetComponent<Collider>(), NYLLON.GetComponent<Collider>());
+        Physics.IgnoreCollision(LEAATHER.GetComponent<Collider>(),COOTTON.GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
