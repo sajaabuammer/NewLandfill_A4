5bede4ac54bc1e00fd1d3d54a47d8402888823f6
SalmaAbdelhady25 <sabde4@uic.edu>
2023-10-14 16:15:15 -0500

    Update (Salma)

---
+++ NewLandfill_A4/Assets/Z-Script trial/WheelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+
+    float rotSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 30;
+        }
+
+        transform.Rotate(0, 0, this.rotSpeed);
+        this.rotSpeed *= 0.16f;
+    }
+}
